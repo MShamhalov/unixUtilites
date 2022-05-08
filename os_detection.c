98c4bca0174c11d2496c38ed591d14b46b9b5c4c
Magomed Shamhalov <m@shamhalov.ru>
2022-05-08 22:29:08 +0300

    fix some bug ;)

--- os_detection.c
+++ os_detection.c
@@ -1,6 +1,12 @@
-#ifdef _WIN64
-    printf ("Windows 64");
-#elif _FreeBSD_
-    printf ("FreeBSD");
-#elif __linux
-    printf ("Linux");
+#include <stdio.h>
+int main(void){
+    #ifdef _WIN64
+        printf ("Windows 64");
+    #elif _FreeBSD_
+        printf ("FreeBSD");
+    #elif __linux
+        printf ("Linux");
+    #endif
+
+    return 0;
+}
