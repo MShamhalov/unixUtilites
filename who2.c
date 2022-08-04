ea565532855e3867db0701e01496c85138fe1f19
Magomed Shamhalov <m@shamhalov.ru>
2022-08-04 11:49:09 +0300

    add who2.changes

---
+++ who2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <utmp.h>
+#include <fcntl.h>
+#include <time.h>
+/* #define SHOWHOST */
+void showtime(long);
+void show_info (struct utmp *);
+int main(){
+     
+}
